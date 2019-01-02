8b038794df2b91399fd1e63040db4cd4de85b86a
Romain PERRIN <romain.perrin@etu.unistra.fr>
2019-01-02 15:00:39 +0100

    ajout d'une nouvelle classe Vertex

---
+++ include/Vertex.h
@@ -0,0 +1,34 @@
+#ifndef __VERTEX_H__
+#define __VERTEX_H__
+
+// needing a 3D vector class
+
+// class representing a vertex : a unique ID and  X Y Z coordinates
+template<typename T>
+class Vertex
+{
+public:
+	//////////////////////////////////////////////////////////////////////////////
+	// attributes
+	//////////////////////////////////////////////////////////////////////////////
+	
+	// unique ID cor comparisons
+	static unsigned long long _idgen;	// should be enough...(>= 2^64)
+	unsigned long long _id;	// unique ID
+
+	// X Y Z coordinates
+	vec3<T> _coord;
+
+	//////////////////////////////////////////////////////////////////////////////
+	// methods
+	//////////////////////////////////////////////////////////////////////////////	
+
+	// constructor
+	Vertex();
+	Vertex(T x, T y, T z);
+};
+
+#include "Vertex.hxx"
+
+#endif
+
